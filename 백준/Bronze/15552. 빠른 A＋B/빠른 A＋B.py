510495b1fcc9463ff95695d4010bab17883316aa
DaeHwan CHOI <74276716+choidaehwan@users.noreply.github.com>
2023-07-07 21:27:08 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1456 ms, Memory: 70224 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,12 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+result = []
+
+for i in range(T):
+    a, b = map(int, sys.stdin.readline().split())
+    result.append(a + b)
+
+for i in result:
+    print(i)
