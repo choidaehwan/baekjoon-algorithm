f5823f9479c96f430b3223acac2b3175706cc99c
DaeHwan CHOI <74276716+choidaehwan@users.noreply.github.com>
2023-07-12 13:53:38 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,18 +1,10 @@
 import sys
 
-submitter = []
+all_student = [i for i in range(1, 31)]
 
 for _ in range(28):
-    i = int(sys.stdin.readline())
-    submitter.append(i)
+    subbmitter = int(sys.stdin.readline())
+    all_student.remove(subbmitter)
 
-not_subbiter = []
-
-for j in range(1, 31):
-    if j not in submitter:
-        not_subbiter.append(j)
-
-not_subbiter.sort()
-
-for n in not_subbiter:
-    print(n)
+print(min(all_student))
+print(max(all_student))
