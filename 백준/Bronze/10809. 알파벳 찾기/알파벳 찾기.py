04b0718a63406c2828c54ba2cefadd1d12880ab4
DaeHwan CHOI <74276716+choidaehwan@users.noreply.github.com>
2023-07-19 19:15:37 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,16 +1,10 @@
 import sys
 
 S = sys.stdin.readline().rstrip()
-
 alphabet = [chr(i) for i in range(97, 123)]
-alphabet_count = [-1 for n in range(26)]
-avoid_duplication = []
 
-for i, s in enumerate(S):
-    if s not in avoid_duplication and s in alphabet:
-        avoid_duplication.append(s)
-        order = ord(s) - 97
-        alphabet_count[order] = i
-
-for i in alphabet_count:
-    print(i, end = ' ')
+for a in alphabet:
+    if a in S:
+        print(S.index(a), end = ' ')
+    else:
+        print(-1, end= ' ')
