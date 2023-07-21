72a597f75372e5401cc873aee28ce06ecd94a415
DaeHwan CHOI <74276716+choidaehwan@users.noreply.github.com>
2023-07-21 16:21:45 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,9 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+for i in range(1, N + 1):
+    print((' ' * (N - i)) + ('*' * i) + ('*' * (i - 1)))
+
+for i in range(N - 1, 0, -1):
+    print((' ' * (N - i)) + ('*' * i) + ('*' * (i - 1)))
