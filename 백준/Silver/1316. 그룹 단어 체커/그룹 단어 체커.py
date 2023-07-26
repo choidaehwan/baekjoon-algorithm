9ef623df9ac03339813b38be0f28b4871c6a7c29
DaeHwan CHOI <74276716+choidaehwan@users.noreply.github.com>
2023-07-26 14:49:36 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -2,19 +2,16 @@ import sys
 
 T = int(sys.stdin.readline())
 
-sum = 0
-for i in range(T):
-    word = sys.stdin.readline().rstrip()
-    only_new = [0]
+sum = T
 
-    for w in word:
-        if w == only_new[-1]:
-            continue
-        elif w not in only_new:
-            only_new.append(w)
-            continue
-        else:
-            T -= 1
+for _ in range(T):
+    word = sys.stdin.readline().rstrip()
+
+    for i in range(len(word) - 1):
+        if word[i] == word[i + 1]:
+            pass
+        elif word[i] in word[i + 1:]:
+            sum -= 1
             break
 
-print(T)
+print(sum)
