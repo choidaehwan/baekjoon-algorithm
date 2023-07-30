7d09ec3426100e81c451aa5f3945d744d04d9a45
DaeHwan CHOI <74276716+choidaehwan@users.noreply.github.com>
2023-07-30 09:38:21 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -2,15 +2,10 @@ import sys
 
 table = [list(map(str, sys.stdin.readline().rstrip())) for _ in range(5)]
 
-for i in range(5):
-    lack = 15 - len(table[i])
-    for _ in range(lack):
-        table[i].append('*')
-
 result = ''
 for i in range(15):
     for j in range(5):
-        if table[j][i] == '*': 
+        if len(table[j]) <= i:
             pass
         else:
             result += table[j][i]
