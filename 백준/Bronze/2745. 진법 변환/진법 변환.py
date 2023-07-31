3f7e68c12778a4c87809663750fff5b63cf1b10f
DaeHwan CHOI <74276716+choidaehwan@users.noreply.github.com>
2023-08-01 08:07:28 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,18 +1,5 @@
 import sys
-import math
 
 N, B = map(str, sys.stdin.readline().split())
 
-def change(num):
-    if ord("A") <= ord(num) <= ord("Z"):
-        return ord(num) - ord("A") + 10
-    else:
-        return int(num)
-
-sum = 0
-i = 0
-for n in N[::-1]:
-    sum += change(n) * math.pow(int(B), i)
-    i += 1
-
-print(int(sum))
+print(int(N, int(B)))
